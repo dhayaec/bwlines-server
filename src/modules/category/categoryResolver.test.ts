2d6c9cf2df4c9442cc472c936c5334f0998d1dd9
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-08-27 16:16:55 +0530

    add more category tests

--- src/modules/category/categoryResolver.test.ts
+++ src/modules/category/categoryResolver.test.ts
@@ -17,6 +17,8 @@ afterAll(async () => connection && connection.close());
 describe('category resolver', () => {
   const name = 'Information Technology ' + Math.random();
   const slug = makeSlug(name);
+  const child = 'Child';
+  const childSlug = makeSlug(child);
 
   const addCategoryTestCase = {
     caseId: 'addCategory',
@@ -31,6 +33,31 @@ describe('category resolver', () => {
     },
   };
 
+  const addCategory2TestCase = {
+    caseId: 'addCategory2',
+    query: `mutation { addCategory(name:"${child}", parentId:1 ){
+        id
+        name
+        slug
+      }}`,
+    expectation: (result: any) => {
+      expect(result.data!.addCategory.name).toEqual(child);
+      expect(result.data!.addCategory.slug).toEqual(childSlug);
+    },
+  };
+
+  const addCategory3TestCase = {
+    caseId: 'addCategory3',
+    query: `mutation { addCategory(name:"${child}", parentId:0 ){
+        id
+        name
+        slug
+      }}`,
+    expectation: (result: any) => {
+      expect(result.errors![0].message).toEqual('Invalid parent');
+    },
+  };
+
   const listMainCategoriesTestCase = {
     caseId: 'listMainCategories',
     query: `{ listMainCategories{ slug } }`,
@@ -41,7 +68,74 @@ describe('category resolver', () => {
     },
   };
 
-  const cases = [addCategoryTestCase, listMainCategoriesTestCase];
+  const getCategoryByIdTestCase = {
+    caseId: 'getCategoryById',
+    query: `{ getCategoryById(id:"1"){ slug } }`,
+    expectation: (result: any) => {
+      expect(result).toEqual({
+        data: { getCategoryById: { slug } },
+      });
+    },
+  };
+
+  const getBreadCrumbPathTestCase = {
+    caseId: 'getBreadCrumbPath',
+    query: `{ getBreadCrumbPath(id:"2"){ slug, parent{
+      slug
+    } } }`,
+    expectation: (result: any) => {
+      expect(result).toEqual({
+        data: {
+          getBreadCrumbPath: {
+            slug: childSlug,
+            parent: {
+              slug,
+            },
+          },
+        },
+      });
+    },
+  };
+
+  const getBreadCrumbPath2TestCase = {
+    caseId: 'getBreadCrumbPath2',
+    query: `{ getBreadCrumbPath(id:"0"){ slug, parent{
+      slug
+    } } }`,
+    expectation: (result: any) => {
+      expect(result.errors![0].message).toEqual('Invalid category');
+    },
+  };
+
+  const getChildCategoriesTestCase = {
+    caseId: 'getChildCategories',
+    query: `{ getChildCategories(id:"1"){ slug children{ slug } } }`,
+    expectation: (result: any) => {
+      expect(result).toEqual({
+        data: { getChildCategories: { slug, children: [{ slug: childSlug }] } },
+      });
+    },
+  };
+
+  const getChildCategories2TestCase = {
+    caseId: 'getChildCategories2',
+    query: `{ getChildCategories(id:"0"){ slug children{ slug } } }`,
+    expectation: (result: any) => {
+      expect(result.errors![0].message).toEqual('Invalid category');
+    },
+  };
+
+  const cases = [
+    addCategoryTestCase,
+    addCategory2TestCase,
+    addCategory3TestCase,
+    listMainCategoriesTestCase,
+    getCategoryByIdTestCase,
+    getChildCategoriesTestCase,
+    getChildCategories2TestCase,
+    getBreadCrumbPathTestCase,
+    getBreadCrumbPath2TestCase,
+  ];
 
   cases.forEach(c => {
     const { query, expectation } = c;
